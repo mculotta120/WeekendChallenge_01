c5da54972ec3da5eae7a65673d0e558e21fac902
mculotta120 <margaret.culotta@gmail.com>
2016-05-28 14:44:53 -0500

    output to html working

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var allEmployees = [];
 //adds employee to allEmloyees array
 var addEmployee = function(){
      //console.log("testing");
-     //take in variables from form
+     //take in values from form and make variables
     var lastName= document.getElementById('lastNameIn').value;
     var firstName= document.getElementById('firstNameIn').value;
     var employeeNum= document.getElementById('employeeNumIn').value;
@@ -21,4 +21,13 @@ var addEmployee = function(){
      // adds individual object to employees array
     allEmployees.push( individual );
     console.log( 'added Employee ' + individual.lastName);
+    listEmployees();
 };
+
+  var listEmployees = function(){
+    document.getElementById('output').innerHTML=' ';
+    for( var i=0; i < allEmployees.length; i++){
+      var line = allEmployees[i].firstName+ " " + allEmployees[i].lastName;
+       document.getElementById('output').innerHTML += '<p>' + line + '</p>';
+     }
+  };
