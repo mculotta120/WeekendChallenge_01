658a8b5d5ce603767daefadb0bb701be1aa2e7e0
mculotta120 <margaret.culotta@gmail.com>
2016-05-28 12:23:56 -0500

    function started working with push

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var allEmployees = [];
 var addEmployee = function(){
-     console.log('testing');
-    // var lastName= document.getElementById('lastNameIn').value;
-    // var firstName= document.getElementById('firstNameIn').value;
-    // var employeeNum= document.getElementById('employeeNumIn').value;
-    // var employeeTitle= document.getElementById('employeeTitleIn').value;
-    // var annualSalary= document.getElementById('annualSalaryIn').value;
-    //
-    // var individual = {
-    //    'lastName' : lastName,
-    //    'firstName' : firstName,
-    //    'number' : employeeNum,
-    //    'title' : employeeTitle,
-    //    'salary' : annualSalary
-    //
-    // };
-    // allEmployees.push( individual );
-    // console.log( 'added Employee' + individual.name);
+     //console.log("testing");
+    var lastName= document.getElementById('lastNameIn').value;
+    var firstName= document.getElementById('firstNameIn').value;
+    var employeeNum= document.getElementById('employeeNumIn').value;
+    var employeeTitle= document.getElementById('employeeTitleIn').value;
+    var annualSalary= document.getElementById('annualSalaryIn').value;
+
+    var individual = {
+       'lastName' : lastName,
+       'firstName' : firstName,
+       'number' : employeeNum,
+       'title' : employeeTitle,
+       'salary' : annualSalary
+
+    };
+    allEmployees.push( individual );
+    console.log( 'added Employee ' + individual.lastName);
 };
