c8f21a3916c7e86ef247b57dc2cde17eb56501a6
mculotta120 <margaret.culotta@gmail.com>
2016-05-28 12:35:58 -0500

    added comments to js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+//empty array of all employees
 var allEmployees = [];
+//adds employee to allEmloyees array
 var addEmployee = function(){
      //console.log("testing");
+     //take in variables from form
     var lastName= document.getElementById('lastNameIn').value;
     var firstName= document.getElementById('firstNameIn').value;
     var employeeNum= document.getElementById('employeeNumIn').value;
     var employeeTitle= document.getElementById('employeeTitleIn').value;
     var annualSalary= document.getElementById('annualSalaryIn').value;
-
+      //turns employee info into object
     var individual = {
        'lastName' : lastName,
        'firstName' : firstName,
@@ -15,6 +18,7 @@ var addEmployee = function(){
        'salary' : annualSalary
 
     };
+     // adds individual object to employees array
     allEmployees.push( individual );
     console.log( 'added Employee ' + individual.lastName);
 };
