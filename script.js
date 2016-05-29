d5f919bccdaad178458eadace97b70eda4fddeef
mculotta120 <margaret.culotta@gmail.com>
2016-05-29 12:28:52 -0500

    salary calculator working kindof...

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //empty array of all employees
 var allEmployees = [];
+//empty array of all the salaries
+var allSalaries = [];
 //adds employee to allEmloyees array
 var addEmployee = function(){
      //console.log("testing");
@@ -18,28 +20,32 @@ var addEmployee = function(){
        'salary' : annualSalary
 
     };
-
+      // var individualSalary = {
+      //   'salary' : annualSalary
+      // };
 
      // adds individual object to employees array
     allEmployees.push( individual );
-    console.log( 'added Employee ' + individual.lastName);
+    allSalaries.push( parseInt(annualSalary) );
+    // console.log( 'added Employee ' + individual.lastName);
 
     listEmployees();
     listSalary();
+
 };
     //lists the employee information under the inputs
     var listEmployees = function(){
     document.getElementById('EmployeeInfoOutput').innerHTML=' ';
     for( var i=0; i < allEmployees.length; i++){
-      var employeeInfoLine ="<b>Employee Name: </b>" + allEmployees[i].lastName+ "<b>, </b>" + allEmployees[i].firstName + "<b> Title: </b>" + allEmployees[i].jobTitle + "<b>  # </b>" + allEmployees[i].number + "<b> Salary: $</b>" + allEmployees[i].salary;
+      var employeeInfoLine ="<b>Employee Name: </b>" + allEmployees[i].lastName+ "<b>, </b>" + allEmployees[i].firstName + "<b> Title: </b>" + allEmployees[i].jobTitle + "<b> Employee # </b>" + allEmployees[i].number + "<b> Salary: $</b>" + allEmployees[i].salary;
        document.getElementById('EmployeeInfoOutput').innerHTML += '<p>' + employeeInfoLine + '</p>';
      }
     };
     //lists total salary info
   var listSalary = function(){
     document.getElementById('salaryInfoOutput').innerHTML = ' ';
-    for(var j=0; j<allEmployees.length; j++){
-      var totalSalaries = (allEmployees[j].salary);
+    for(var j=0; j<allSalaries.length; j++){
+      var totalSalaries = allSalaries;
       var monthlySalaries = totalSalaries/12;
       var salaryInfoLine ='<b>Total of all Salaries this Month: </b>'+ monthlySalaries + '<b> Total of all Annual Salaries: </b>' + totalSalaries;
       document.getElementById('salaryInfoOutput').innerHTML += '<p>' + salaryInfoLine + '</p>';
